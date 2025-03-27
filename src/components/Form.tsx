cff07169844b0ddfb171110a59f2956393ed99ff
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-03-27 23:53:06 +0530

    Implement form validation for name using react-hook-form and zod; refactor form submission handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,28 @@
-import { Alert, Autocomplete, Button, Checkbox, FormControlLabel, Grid } from "@mui/material";
+import {
+  Alert,
+  Autocomplete,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const FormGrid = styled(Grid)(() => ({
   display: "flex",
   flexDirection: "column",
 }));
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+ 
+});
 
+type FormData = z.infer<typeof schema>;
 
 // Define the currency options
 const currency = [
@@ -18,69 +32,92 @@ const currency = [
 ];
 
 const accountType = [
-    { label: "Savings" },
-    { label: "Current" },
-    { label: "Fixed Deposit" },
+  { label: "Savings" },
+  { label: "Current" },
+  { label: "Fixed Deposit" },
 ];
 
-export default function Form() {
-  const [name, setName] = useState("");
-  return (
-    <Grid container spacing={2}>
-      <FormGrid size={{ xs: 12 }}>
-        <TextField label="Full name" id="fullName" />
-      </FormGrid>
-      <FormGrid size={{ xs: 12 }}>
-        <TextField label="Email address" id="email" />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <TextField label="Phone Number" id="phoneNumber" type="number" />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <TextField
-          fullWidth
-          label="Date of Birth"
-          id="dob"
-          type="date"
-          InputLabelProps={{ shrink: true }}
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <Autocomplete
-          disablePortal
-          options={accountType}
-          renderInput={(params) => (
-            <TextField {...params} label="Account Type" />
-          )}
-        />{" "}
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <TextField label="Initial Deposit Amount" id="deposit" type="number" />
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <Autocomplete
-          disablePortal
-          options={currency}
-          renderInput={(params) => <TextField {...params} label="Currency" />}
-        />{" "}
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <TextField fullWidth label="Street Address" id="street" />
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <TextField fullWidth label="City" id="city" />
-      </FormGrid>
-      <FormGrid size={{ xs: 4 }}>
-        <TextField fullWidth label="Zip Code" id="zip" type="number" />
-      </FormGrid>
-      <FormGrid size={{ xs: 12 }}>
-        <FormControlLabel
-          control={<Checkbox name="submitForm" value="yes" />}
-          label="I agree to the Terms & Conditions"
-        />
-      </FormGrid>
+const onSubmit = (data: FieldValues) => {
+  console.log(data);
+};
 
-      <Button variant="contained" onClick={() => setName("Button Clicked")}>Submit</Button>
-    </Grid>
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <FormGrid size={{ xs: 12 }}>
+          <TextField {...register("name")} label="Full name" id="fullName" />
+          {errors.name && <Alert severity="error">{errors.name.message}</Alert>}
+        </FormGrid>
+        <FormGrid size={{ xs: 12 }}>
+          <TextField  label="Email address" id="email" />
+
+    
+        </FormGrid>
+        <FormGrid size={{ xs: 6 }}>
+          <TextField
+            label="Phone Number"
+            id="phoneNumber"
+            type="number"
+          />
+        </FormGrid>
+        <FormGrid size={{ xs: 6 }}>
+          <TextField
+            fullWidth
+            label="Date of Birth"
+            id="dob"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+          />
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <Autocomplete
+            disablePortal
+            options={accountType}
+            renderInput={(params) => (
+              <TextField {...params} label="Account Type" />
+            )}
+          />{" "}
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <TextField
+            label="Initial Deposit Amount"
+            id="deposit"
+            type="number"
+          />
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <Autocomplete
+            disablePortal
+            options={currency}
+            renderInput={(params) => <TextField {...params} label="Currency" />}
+          />{" "}
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <TextField fullWidth label="Street Address" id="street" />
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <TextField fullWidth label="City" id="city" />
+        </FormGrid>
+        <FormGrid size={{ xs: 4 }}>
+          <TextField fullWidth label="Zip Code" id="zip" type="number" />
+        </FormGrid>
+        <FormGrid size={{ xs: 12 }}>
+          <FormControlLabel
+            control={<Checkbox name="submitForm" value="yes" />}
+            label="I agree to the Terms & Conditions"
+          />
+        </FormGrid>
+
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </form>
   );
 }
